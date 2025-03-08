c897c51a892f1635315d7a4ce42a1a582a337d22
AWBuchanan7 <buchananaubrey7@protonmail.ch>
2025-03-08 07:25:10 -0800

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
 	name: 'Aubrey',
 	imageBackground: true,
 	openInNewTab: true,
-	twelveHourFormat: false,
+	twelveHourFormat: true,
 
 	// Greetings
 	greetingMorning: 'おはよう!',
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '437f6c71028986fda571c0ee633c5049', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '49.2604134',
+	defaultLongitude: '123.1139456',
 
 	// Autochange
 	autoChangeTheme: true,
