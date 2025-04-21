7e2c3f67540c53993f866f11415e8d3a43d4181e
AWBuchanan7 <buchananaubrey7@protonmail.ch>
2025-04-21 09:30:28 -0700

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '49.2604134',
-	defaultLongitude: '123.1139456',
+	defaultLatitude: '123.1139456',
+	defaultLongitude: '49.2604134',
 
 	// Autochange
 	autoChangeTheme: true,
